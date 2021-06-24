d5b36bbf0a528b25d431aa85562bc1abe14ed5b5
ShwethaNayak <shwetha.patla@gmail.com>
2021-06-24 19:33:54 +0530

    added vegetables

--- greetings.py
+++ greetings.py
@@ -1,4 +1,9 @@
 print('hello everybody')
 fruits=['apple','grapes','orange']
 print(fruits)
+
+#adding veggies
+vegetables=['tomato','potato','palak']
+print(vegetables)
+
 print('adding new line here')
